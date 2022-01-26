9827bc12cae1e4785b68ad53993162816816e33e
hadidbilla <hadid15-13297@diu.edu.bd>
2022-01-26 20:02:14 +0600

    second commit

---
+++ layouts/default.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
+    <Nuxt />
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/Navigation/TheHeader";
+import TheSidenav from "@/components/Navigation/TheSidenav";
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav,
+  },
+  data() {
+    return {
+      displaySidenav: false,
+    };
+  },
+};
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
